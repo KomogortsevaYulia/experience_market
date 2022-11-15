9fb96463576ab78f24d25a462a10c5e13003760c
Yulia Komogortseva <komog-y-v@yandex.ru>
2022-11-16 02:12:39 +0800

    add connection to BD and add migrations

--- project/server/src/app.module.ts
+++ project/server/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import CONNECTION from './db.connection';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProjectsModule } from './projects/projects.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    //@ts-ignore
+    TypeOrmModule.forRoot({
+      ...CONNECTION,
+      synchronize: false,
+      autoLoadEntities: true
+    }),
+    ProjectsModule,
+    UsersModule,],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
